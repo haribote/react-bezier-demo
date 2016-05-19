317b8142f90f54d10c89903052fb13c127ded29a
KimuraTetsuro <haribote@Harrys-MBA-Dash.local>
2016-05-19 12:49:04 +0900

    add: components/app.jsx

---
+++ src/components/app.jsx
@@ -0,0 +1,25 @@
+// import modules
+import React from 'react';
+
+/**
+ * @export App
+ */
+export default class App extends React.Component {
+  /**
+   * @constructor
+   * @param props
+   */
+  constructor(props) {
+    super(props);
+  }
+
+  /**
+   * @method renderer
+   * @returns {XML}
+   */
+  render() {
+    return (
+      <h1>React Bezier Demo</h1>
+    );
+  }
+};
